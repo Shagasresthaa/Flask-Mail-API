effeb698e7c486167e414c52dfcd1a4a811fae42
Shaga Sresthaa <shagasresthaa@gmail.com>
2020-12-17 15:10:08 +0530

    minor fixes

--- App.py
+++ App.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 app.config['TESTING'] = False
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_SERVER'] = '***********'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 #app.config['MAIL_DEBUG'] = True
-app.config['MAIL_USERNAME'] = 'sresthaafeedback@gmail.com'
-app.config['MAIL_PASSWORD'] = '@5Sresthaa1'
-app.config['MAIL_DEFAULT_SENDER'] = 'sresthaafeedback@gmail.com'
+app.config['MAIL_USERNAME'] = '**************'
+app.config['MAIL_PASSWORD'] = '**************'
+app.config['MAIL_DEFAULT_SENDER'] = '******************'
 app.config['MAIL_MAX_EMAILS'] = 'None'
 #app.config['MAIL_SUPPRESS_SEND'] = False
 app.config['MAIL_ASCII_ATTACHMENTS'] = False
@@ -23,8 +23,8 @@ mail = Mail(app)
 @app.route('/')
 def index():
     msg = Message('Feedback/Collab Contact Request', recipients=[
-        'shagasresthaa@gmail.com'])
-    msg.html = '<b>Hello Sresthaa,<br>This is a test request from your contact form</b>'
+        '********************'])
+    msg.html = '<b>Hello ********,<br>This is a test request from your contact form</b>'
     mail.send(msg)
 
     return 'Message sent'
